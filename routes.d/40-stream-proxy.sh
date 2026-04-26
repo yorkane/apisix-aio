ac4276e596587c75171b92fdd73a4aaa3a733c0f
yorkane <yorkane@github.com>
2026-04-26 11:21:23 +0000

    feat: add put_stream_route helper and configure tcp-60001 proxy

--- routes.d/40-stream-proxy.sh
+++ routes.d/40-stream-proxy.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 # =============================================================================
-# 40-stream-proxy.sh - Dynamic Stream (TCP/UDP) routes
+# 40-stream-proxy.sh - Stream (TCP/UDP) proxy routes
 # =============================================================================
-# Manage stream routes via Admin API.
-# Ports 60001-60009 are pre-allocated in apisix_config.yml for this purpose.
 
-# TCP Proxy: Local 60001 -> 144.168.59.25:27777
 put_stream_route "tcp-60001" '{
   "server_port": 60001,
   "upstream": {
